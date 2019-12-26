4c993d69bc25273f7665af4a188dfa17b2836291
Chinraj M <chinraj044@gmail.com>
2019-12-26 13:55:01 +0530

    Update chin.sql

--- chin.sql
+++ chin.sql
@@ -1,3 +1,4 @@
+drop table task;
 create table task(
 task_name varchar2(30),
 done_by varchar2(30),
@@ -7,6 +8,6 @@ status varchar2(20)
 );
 insert into task (task_name, done_by, deadline, done_on,status)
 values('instal oracle','naresh',to_date('26-12-2019','dd-MM-yyyy'),to_date('25-12-2019','dd-MM-yyyy'),'completed');
-insert into task (task_name, done_by, done_on,status)
-values('instal jdk','siva',to_date('20-01-2020','dd-MM-yyyy'),'incomplete');
+insert into task (task_name, done_by, deadline,status)
+values('instal jdk','siva',to_date('26-01-2020','dd-MM-yyyy'),'incomplete');
 select* from task;
